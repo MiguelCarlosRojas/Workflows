ff55fbf90d862025a46150af0f20a78455e875e5
Michael JQC <105119643+miquidev@users.noreply.github.com>
2024-12-02 14:05:52 +0000

    v18

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+var App = function () {
+    return (React.createElement("div", { style: { textAlign: 'center', marginTop: '50px' } },
+        React.createElement("h1", null, "Welcome to My TypeScript Application"),
+        React.createElement("p", null, "This is a basic setup for a React app with TypeScript!")));
+};
+export default App;
